3c44f1af0af3574e73d8d303af1314fbe2715b74
Pranav Budhwant <prnvb15@gmail.com>
2019-07-07 09:28:42 +0530

    CoordConv, Pix2Pix
    1. CoordConv has been implemented in the CNN Autoencoder, the results are not promising.
    2. Fixed pix2pix implementation

--- build_cnn_autoencoder_model.py
+++ build_cnn_autoencoder_model.py
@@ -6,44 +6,59 @@ import numpy as np
 import sys
 np.set_printoptions(threshold=sys.maxsize)
 
-from keras.layers import Conv2D, MaxPooling2D, UpSampling2D
+from keras.layers import Conv2D, MaxPooling2D, UpSampling2D, Conv2DTranspose, ZeroPadding2D
 from keras import backend as K
 
-def build_cnn_autoencoder_model(profile_pngs_gray_objs, midcurve_pngs_gray_objs,encoding_dim = 100, input_dim = 100):
-    input_img = Input(shape=(input_dim, input_dim, 1))  # adapt this if using `channels_first` image data format
+def build_cnn_autoencoder_model(profile_pngs_gray_objs, midcurve_pngs_gray_objs,encoding_dim = 100, input_dim = 128):
     
-    x = Conv2D(16, (3, 3), activation='relu', padding='same')(input_img)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    x = MaxPooling2D((2, 2), padding='same')(x)
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
-    encoded = MaxPooling2D((2, 2), padding='same')(x)
+    input_img = Input(shape=(input_dim, input_dim, 3))  # adapt this if using `channels_first` image data format
+    
+    #x = ZeroPadding2D((5,5))(input_img)
+    x = Conv2D(32, (3, 3), strides=(2, 2), activation='relu', padding='same')(input_img)
+    #x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+    #x = MaxPooling2D((2, 2), padding='same')(x)
+    x = Conv2D(16, (3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+    x = Conv2D(8, (3, 3), strides=(2, 2), activation='relu', padding='same')(x)
+    encoded = Conv2D(8, (3, 3), strides=(2,2), activation='relu', padding='same')(x)
+    #
+    #encoded = MaxPooling2D((2, 2), padding='same')(x)
 
     # at this point the representation is (4, 4, 8) i.e. 128-dimensional
     
-    x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
+    '''x = Conv2D(8, (3, 3), activation='relu', padding='same')(encoded)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(8, (3, 3), activation='relu', padding='same')(x)
     x = UpSampling2D((2, 2))(x)
     x = Conv2D(16, (3, 3), activation='relu')(x)
     x = UpSampling2D((2, 2))(x)
-    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)
+    decoded = Conv2D(1, (3, 3), activation='sigmoid', padding='same')(x)'''
+    #y = ZeroPadding2D()(encoded)
+    y = Conv2DTranspose(8, (3, 3), strides = (2, 2), padding='same', activation='relu')(encoded)
+    y = Conv2DTranspose(4, (3, 3), strides = (2, 2), padding='same', activation='relu')(y)
+    y = Conv2DTranspose(4, (3, 3), strides = (2, 2), padding='same', activation='relu')(y)
+    y = Conv2DTranspose(2, (3, 3), strides = (2, 2), padding='same', activation='relu')(y)
+    decoded = Conv2DTranspose(1, (3, 3), strides = (2, 2), activation='tanh', padding='same')(y)
     
     
     # Model 1: Full AutoEncoder, includes both encoder single dense layer and decoder single dense layer. 
     # This model maps an input to its reconstruction
     autoencoder = Model(input_img, decoded)
+    autoencoder.summary()
     
     # Compilation of Autoencoder (only)
-    autoencoder.compile(optimizer='adadelta', loss='binary_crossentropy')
+    autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
     
     
     # Training
-    profile_pngs_flat_objs = [x.reshape(input_dim,input_dim,1) for x in profile_pngs_gray_objs]
-    midcurve_pngs_flat_objs = [x.reshape(input_dim,input_dim,1) for x in midcurve_pngs_gray_objs]
+    #profile_pngs_flat_objs = [x.reshape(input_dim,input_dim,1) for x in profile_pngs_gray_objs]
+    #midcurve_pngs_flat_objs = [x.reshape(input_dim,input_dim,1) for x in midcurve_pngs_gray_objs]
     
-    profile_pngs_objs = np.array(profile_pngs_flat_objs)
-    midcurve_pngs_objs= np.array(midcurve_pngs_flat_objs)
+    #profile_pngs_objs = np.array(profile_pngs_flat_objs)
+    #midcurve_pngs_objs= np.array(midcurve_pngs_flat_objs)
+
+    profile_pngs_objs = profile_pngs_gray_objs
+    midcurve_pngs_objs = midcurve_pngs_gray_objs
     
     train_size = int(len(profile_pngs_objs)*0.7)
     x_train = profile_pngs_objs[:train_size]
--- main_cnn_autoencoder.py
+++ main_cnn_autoencoder.py
@@ -2,7 +2,35 @@ from prepare_data import get_training_data
 from prepare_plots import plot_results
 from build_cnn_autoencoder_model import build_cnn_autoencoder_model
 
+import numpy as np
+
 if __name__ == "__main__":
-    profile_pngs_objs, midcurve_pngs_objs = get_training_data()
-    original_imgs,decoded_imgs = build_cnn_autoencoder_model(profile_pngs_objs, midcurve_pngs_objs)
+    profile_pngs_objs, midcurve_pngs_objs = get_training_data(size = (128, 128))
+
+    profile_pngs_objs = np.asarray(profile_pngs_objs)    
+    midcurve_pngs_objs = np.asarray(midcurve_pngs_objs)    
+    
+    profile_pngs_objs = np.expand_dims(profile_pngs_objs, axis=-1)
+    midcurve_pngs_objs = np.expand_dims(midcurve_pngs_objs, axis=-1)
+
+    profile_pngs_objs = (profile_pngs_objs - 127.5)/127.5 #Normalize [-1, 1]
+    midcurve_pngs_objs = (midcurve_pngs_objs - 127.5)/127.5 #Normalize [-1, 1]
+    
+    x_coord = np.zeros(shape=(128, 128, 1))
+    y_coord = np.zeros(shape=(128, 128, 1))
+    for i in range(0, 128):
+        x_coord[:, i, 0] = i
+        y_coord[i, :, 0] = i
+    coords = np.append(x_coord, y_coord, axis=-1)
+    coords = (coords - 63.5)/63.5 #Normalize [-1, 1]
+    #coords = np.expand_dims(coords, axis=0)
+    
+    profile_pngs = []
+    
+    for i in range(len(profile_pngs_objs)):
+        profile_pngs.append(np.append(profile_pngs_objs[i], coords, axis=-1))
+    
+    profile_pngs = np.asarray(profile_pngs)
+    
+    original_imgs,decoded_imgs = build_cnn_autoencoder_model(profile_pngs, midcurve_pngs_objs)
     plot_results(original_imgs,decoded_imgs)
