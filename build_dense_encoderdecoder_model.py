b9d97bd926f22627dfd13bbe413910bc12932f45
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2019-07-16 09:42:39 +0530

    Training on full data

--- build_dense_encoderdecoder_model.py
+++ build_dense_encoderdecoder_model.py
@@ -6,6 +6,8 @@ from prepare_plots import plot_results
 import numpy as np
 import os
 import sys
+import random
+
 np.set_printoptions(threshold=sys.maxsize)
 
 class dense_encoderdecoder:
@@ -69,17 +71,23 @@ class dense_encoderdecoder:
             profile_pngs_objs = self.process_images(profile_pngs_gray_objs)
             midcurve_pngs_objs = self.process_images(midcurve_pngs_gray_objs)
                     
-            train_size = int(len(profile_pngs_objs)*0.7)
-            self.x_train = profile_pngs_objs[:train_size]
-            self.y_train = midcurve_pngs_objs[:train_size]
-            self.x_test = profile_pngs_objs[train_size:]
-            self.y_test = midcurve_pngs_objs[train_size:]
-            self.autoencoder.fit(self.x_train, self.y_train,
+#             train_size = int(len(profile_pngs_objs)*0.7)
+#             self.x_train = profile_pngs_objs[:train_size]
+#             self.y_train = midcurve_pngs_objs[:train_size]
+#             self.x_test = profile_pngs_objs[train_size:]
+#             self.y_test = midcurve_pngs_objs[train_size:]
+#             self.autoencoder.fit(self.x_train, self.y_train,
+#                         epochs=self.epochs,
+#                         batch_size=32,
+#                         shuffle=True,
+#                         validation_data=(self.x_test, self.y_test))
+                
+            self.x = profile_pngs_objs
+            self.y = midcurve_pngs_objs
+            self.autoencoder.fit(self.x, self.y,
                         epochs=self.epochs,
                         batch_size=5,
-                        shuffle=True,
-                        validation_data=(self.x_test, self.y_test))
-                
+                        shuffle=True)                     
             # Save models
             self.autoencoder.save(self.autoencoder_model_pkl)
             self.encoder.save(self.encoder_model_pkl)
@@ -102,6 +110,6 @@ if __name__ == "__main__":
     endec = dense_encoderdecoder()
     endec.train(profile_gray_objs, midcurve_gray_objs)
     
-    test_gray_images = profile_gray_objs[:5]
+    test_gray_images = random.sample(profile_gray_objs,5)
     original_profile_imgs,predicted_midcurve_imgs = endec.predict(test_gray_images)
     plot_results(original_profile_imgs,predicted_midcurve_imgs)
--- main_dense_encoderdecoder.py
+++ main_dense_encoderdecoder.py
@@ -1,8 +1,13 @@
 from prepare_data import get_training_data
 from prepare_plots import plot_results
-from main_dense_encoderdecoder import build_dense_encoderdecoder_model
+from build_dense_encoderdecoder_model import dense_encoderdecoder
+import random
 
 if __name__ == "__main__":
-    profile_pngs_objs, midcurve_pngs_objs = get_training_data()
-    original_imgs,decoded_imgs = build_dense_encoderdecoder_model(profile_pngs_objs, midcurve_pngs_objs)
-    plot_results(original_imgs,decoded_imgs)
+    profile_gray_objs, midcurve_gray_objs = get_training_data()
+    endec = dense_encoderdecoder()
+    endec.train(profile_gray_objs, midcurve_gray_objs)
+    
+    test_gray_images = random.sample(profile_gray_objs,5)
+    original_profile_imgs,predicted_midcurve_imgs = endec.predict(test_gray_images)
+    plot_results(original_profile_imgs,predicted_midcurve_imgs)
