35cd7b981652100c62e0e9607defb6550226ced6
Yogesh Kulkarni <yogeshkulkarni@yahoo.com>
2019-10-05 21:05:35 +0530

    Renamed denoiser dir

--- core/denseencoderdecoder/build_dense_encoderdecoder_model.py
+++ core/denseencoderdecoder/build_dense_encoderdecoder_model.py
@@ -118,8 +118,8 @@ if __name__ == "__main__":
     
     test_gray_images = random.sample(profile_gray_objs,5)
 
-    #profile_gray_objs = np.asarray(profile_gray_objs)/255.
-    #midcurve_gray_objs = np.asarray(midcurve_gray_objs)/255.
-    #test_gray_images = np.asarray(test_gray_images)/255.
+    profile_gray_objs = np.asarray(profile_gray_objs)/255.
+    midcurve_gray_objs = np.asarray(midcurve_gray_objs)/255.
+    test_gray_images = np.asarray(test_gray_images)/255.
     original_profile_imgs,predicted_midcurve_imgs = endec.predict(test_gray_images)
     plot_results(original_profile_imgs,predicted_midcurve_imgs)
